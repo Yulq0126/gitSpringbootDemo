c0ce7887093725af6d5a2d5378d9537e4fd9e24d
leos123 <ylqlove126@163.com>
2020-07-13 11:42:59 +0800

    新建分支

--- good-service/src/main/java/com/ylq/goodService/GoodServiceApplication.java
+++ good-service/src/main/java/com/ylq/goodService/GoodServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GoodServiceApplication {
-
+    private String str="122";
     public static void main(String[] args) {
         SpringApplication.run(GoodServiceApplication.class, args);
     }
